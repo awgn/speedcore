b8a65e453d4991e56c647b43ca9fc3898fbae9bc
Nicola Bonelli <nicola.bonelli@gmail.com>
2012-05-24 19:25:57 +0200

    Removed SPSC queue...

--- speedcore.cpp
+++ speedcore.cpp
@@ -1,22 +1,3 @@
-/*
- *  Copyright (c) 2011 Bonelli Nicola <bonelli@antifork.org>
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- */
-
 #include <iostream>
 #include <fstream>
 #include <stdexcept>
@@ -31,90 +12,7 @@
 
 #include <pthread.h> // pthread_setaffinity_np
 
-struct queue_header
-{
-    struct 
-    {
-        volatile unsigned long index;
-    } producer __attribute__((aligned(64)));
-
-    struct 
-    {
-        volatile unsigned long index;
-    } consumer __attribute__((aligned(64)));
-
-    unsigned long size;             /* number of slots */
-    unsigned long size_mask;        /* number of slots */
-
-} queue_hdr  __attribute__((aligned(64)));
-
-
-int queue_slot[1<<10];
-
-
-#define wmb() asm   __volatile__ ("" ::: "memory") 
-#define likely(x)   __builtin_expect((x),1)
-#define unlikely(x) __builtin_expect((x),0)
-
-
-inline void
-spsc_init(struct queue_header *that, unsigned long s)
-{
-    that->producer.index  = 0;
-    that->consumer.index  = 0;
-
-    that->size = s;
-    that->size_mask = (s-1);
-}
-
-
-inline
-int spsc_write_index(struct queue_header *q)
-{
-    auto size = (q->consumer.index - q->producer.index + q->size_mask) & q->size_mask;
-    if (unlikely(size == 0)) {
-        return -1;
-    }
-    return q->producer.index;
-}
-
-
-inline
-void spsc_write_commit(struct queue_header *q)
-{
-    wmb();
-    auto size = (q->consumer.index - q->producer.index + q->size_mask) & q->size_mask;
-    if (size != 0) {
-        q->producer.index = (q->producer.index + 1) & q->size_mask;
-    }
-    wmb();
-}
-
-
-inline
-int spsc_read_index(struct queue_header *q)
-{   
-    auto size = (q->producer.index - q->consumer.index + q->size) & q->size_mask;
-    if(unlikely(size == 0)) {
-        return -1;
-    }
-    return q->consumer.index;
-}
-
-
-inline
-void spsc_read_commit(struct queue_header *q)
-{
-    wmb();
-    auto size = (q->producer.index - q->consumer.index + q->size) & q->size_mask;
-    if (size != 0) {
-        q->consumer.index = (q->consumer.index + 1) & q->size_mask;
-    }
-    wmb();
-}
-
-
-inline void 
+static inline void 
 set_affinity(std::thread &t, int n) 
 {
     if(t.get_id() == std::thread::id())
@@ -129,8 +27,7 @@ set_affinity(std::thread &t, int n)
 }
 
 
-unsigned int 
-hardware_concurrency()
+unsigned int hardware_concurrency()
 {
     auto proc = []() {
         std::ifstream cpuinfo("/proc/cpuinfo");
@@ -142,41 +39,8 @@ hardware_concurrency()
     return std::thread::hardware_concurrency() ? : proc();
 }
 
-
-std::atomic_bool barrier;
-
-void producer()
-{
-    while(barrier.load());
-
-    for(int i = 0; i < 100000000; i++)
-    {
-        int w;
-        do {   
-            w = spsc_write_index(&queue_hdr);
-        }
-        while(w == -1);
-        queue_slot[w] = i;
-        spsc_write_commit(&queue_hdr);
-    }
-}
-
-
-void consumer()
-{
-    int sum = 0;
-    for(int i = 0; i < 100000000; i++)
-    {
-        int r;
-        do {   
-            r = spsc_read_index(&queue_hdr);
-        }
-        while(r == -1);
-        sum += queue_slot[r];
-        spsc_read_commit(&queue_hdr);
-    }
-}
-
+std::atomic_long p_pipe;
+std::atomic_long c_pipe;
 
 int
 main(int argc, char *argv[])
@@ -193,19 +57,29 @@ main(int argc, char *argv[])
 
             std::cout << "SpeedCore " << "/-\\|"[n&3] << '\r' << std::flush;
 
-            spsc_init(&queue_hdr, sizeof(queue_slot)/sizeof(queue_slot[0]));
+            p_pipe.store(0);
+            c_pipe.store(0);
 
+            std::thread c ([] {
+                for(long i = 1; i < 10000000; i++)
+                {
+                    p_pipe.store(i);
+                    while (c_pipe.load() != i)
+                    {}
+                }});
 
-            barrier.store(true);
-
-            std::thread c (consumer);
-            std::thread p (producer);
+            std::thread p ([] {
+                for(long i = 1; i < 10000000; i++)
+                {
+                    while(p_pipe.load() != i)
+                    {}
+                    c_pipe.store(i);
+                }});
 
             set_affinity(c, i);
             set_affinity(p, j);
 
             auto begin = std::chrono::system_clock::now();
-            barrier.store(false);
 
             c.join();
             p.join();
